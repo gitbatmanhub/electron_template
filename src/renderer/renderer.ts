5fc5b2f475eababff9722a490dc11cc73243a21c
EdwinDev <ecro98.98@gmail.com>
2026-05-09 21:37:02 -0500

    create rendered

--- src/renderer/renderer.ts
+++ src/renderer/renderer.ts
@@ -1,3 +1,40 @@
-window.electronAPI.getVersion().then(version => {
-    alert("Browser support electron")
-});
+window.addEventListener('load', () => {
+    const button = document.getElementById('loadFile');
+    const loadSucursales = document.getElementById('loadSucursales');
+    const listOfSucursales = document.getElementById('listOfSucursales')!;
+    button?.addEventListener('click', async () => {
+        const content = await window.api.file.open();
+        console.log(content)
+    });
+
+    loadSucursales?.addEventListener('click', async () => {
+        await window.api.sucursales.get_sucursales().then(sucursales => {
+            listOfSucursales.innerHTML = '<option value="">Selecciona un usuario</option>';
+
+            sucursales.forEach(sucursal => {
+                console.log(sucursal)
+                const opcion = document.createElement('option');
+                opcion.value = sucursal.idSucursal;
+                opcion.innerText = sucursal.nombre;
+                // opcion.addEventListener('click', async () => {
+                //     await loadServices(sucursal.idSucursal);
+                // })
+                listOfSucursales.appendChild(opcion);
+                listOfSucursales.addEventListener('change', async () => {
+                    await loadServices(sucursal.idSucursal);
+
+                })
+            });
+
+
+        }).catch(error => console.log(error));
+
+    })
+})
+
+
+async function loadServices(idSucursal: string) {
+    await window.api.servicios_sucursales.get_servicios_sucursales().then(sucursales => {
+        console.log(sucursales)
+    })
+}
